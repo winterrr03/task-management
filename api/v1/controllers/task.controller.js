599a6ccaaa1aac12db0fa10db9549b25f3e5b231
winterrr03 <dong.huynhquang21503@gmail.com>
2024-06-22 20:25:10 +0700

    feat: filter, sort

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -2,9 +2,23 @@ const Task = require("../models/task.model");
 
 // [GET] /api/v1/tasks
 module.exports.index = async (req, res) => {
-  const tasks = await Task.find({
+  const find = {
     deleted: false
-  });
+  }
+
+  if (req.query.status) {
+    find.status = req.query.status
+  }
+
+  // Sắp xếp
+  const sort = {};
+
+  if(req.query.sortKey && req.query.sortValue) {
+    sort[req.query.sortKey] = req.query.sortValue;
+  }
+  // Hết Sắp xếp
+
+  const tasks = await Task.find(find).sort(sort);
 
   res.json(tasks);
 };
